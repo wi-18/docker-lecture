b8472f4b9b3a8e9fec2bf85ca6a558d75a57e87b
Bjarne Koll <LynxPlay101@gmail.com>
2020-01-19 15:20:17 +0100

    Create task 1 in the kubernetes task
    This commit creates the kuberentes yaml configuration file for deploying
    a single standalone pod instance to the kubernetes cluster.

--- docker/3-simple-web-hello-world/docker-image/src/main.go
+++ docker/3-simple-web-hello-world/docker-image/src/main.go
@@ -36,6 +36,10 @@ func main() {
 	port := 80
 	data := WebTemplateData{Gif: NoPortGif}
 
+	if _, ok := os.LookupEnv("KUBERNETES_SERVICE_HOST"); ok {
+		fmt.Println("Oh look, we are running inside a kubernetes cluster! Well done!")
+	}
+
 	if envPort, ok := os.LookupEnv("PORT"); ok {
 		if val, err := strconv.Atoi(envPort); err != nil {
 			fmt.Println(fmt.Sprintf("provided environment variable PORT is not an integer (%s)", envPort))
---
+++ kubernetes/1-publish-single-application/instructions.md
@@ -0,0 +1,11 @@
+Erstelle eine erste Applikation
+=====
+
+Das Ziel
+========
+Das erstellen 
+
+Hints
+=====
+Provide a simple hint to ease execution of the task for students.
+
